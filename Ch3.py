b518392f46f36d95e604d82463513f18fdeb4e8b
YKKim2017 <30789697+YKKim2017@users.noreply.github.com>
2017-08-07 14:05:03 +0900

    Update and rename 3.4.3 3F Neuron.py to Ch3.py

--- Ch3.py
+++ Ch3.py
@@ -29,3 +29,12 @@ y = forward(network, x)
 print(y)
 
 
+# 3.5.1 softmax
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    
+    return y
+    
+    
